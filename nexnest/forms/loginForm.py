8a72306ec1dae2b85f1e30c71663f7aea68438ff
maxbbender <maxbbender@gmail.com>
2017-01-08 19:43:42 -0500

    login working

--- nexnest/forms/loginForm.py
+++ nexnest/forms/loginForm.py
@@ -1,5 +1,7 @@
 from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from flask_wtf import FlaskForm
 
-class LoginForm(Form): 
+
+class LoginForm(FlaskForm):
     email = StringField('Email Address', [validators.Length(min=6, max=35)])
     password = PasswordField('Password', [validators.DataRequired()])
