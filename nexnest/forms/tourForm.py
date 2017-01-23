16a2df7c18f163cd77ce3525c26c8f37138ce24d
pmagent2013 <pmagent2013@gmail.com>
2017-01-23 18:08:00 -0500

    Inital Tour Request Form

---
+++ nexnest/forms/tourForm.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+
+from wtforms.fields import HiddenField, StringField, TextAreaField
+
+from wtforms.validators import InputRequired, Length
+
+
+class TourForm(FlaskForm):
+    group_id = HiddenField('group_id', [InputRequired()])
+    listing_id = HiddenField('listing_id', [InputRequired()])
+    description  = TextAreaField('Message to Landlord', [Length(min=1, max=1500), InputRequired()])
+    requested_dateTime = HiddenField('Date and time you would like to tour the house')
