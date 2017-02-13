af49a4b4517917b0a8f1b8b9c3a9206b7994b130
pmagent2013 <pmagent2013@gmail.com>
2017-02-13 18:02:01 -0500

    spacing

--- nexnest/forms.py
+++ nexnest/forms.py
@@ -155,13 +155,13 @@ class DirectMessageForm(RedirectForm):
 
 
 class EditAccountForm(RedirectForm):
-    fname = StringField('First Name<span style="color: red;">(Required)</span>', [InputRequired()])
-    lname = StringField('Last Name<span style="color: red;">(Required)</span>', [InputRequired()])
-    school = StringField('School Attending<span style="color: red;">(Required)</span>', [InputRequired()])
+    fname = StringField('First Name<span style="color: red;"> (Required)</span>', [InputRequired()])
+    lname = StringField('Last Name<span style="color: red;"> (Required)</span>', [InputRequired()])
+    school = StringField('School Attending<span style="color: red;"> (Required)</span>', [InputRequired()])
     dob = StringField('Date of Birth')
     bio = TextAreaField('If you wish provide a short personal bio')
     phone = StringField('Phone Number')
-    email = StringField('Email<span style="color: red;">(Required)</span>',
+    email = StringField('Email<span style="color: red;"> (Required)</span>',
                         [InputRequired("You must enter an email address"),
                          Email("Email must be valid format")])
 
