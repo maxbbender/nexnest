35b3ed18150f623922909d6fd1322e4f0693e43a
Max Bender <maxbbender@gmail.com>
2017-04-14 21:25:59 -0400

    group column change

--- db/versions/048_group_column_add.py
+++ db/versions/048_group_column_add.py
@@ -9,8 +9,9 @@ def downgrade(migrate_engine):
     groups = Table('groups', meta, autoload=True)
 
     # users.c.password.drop()
-    groups.c.start_date.drop()
-    groups.c.end_date.drop()
+    # groups.c.start_date.drop()
+    # groups.c.end_date.drop()
+    groups.c.target_time_period.drop()
     pass
 
 
@@ -21,10 +22,12 @@ def upgrade(migrate_engine):
     groups = Table('groups', meta, autoload=True)
 
     # password = Column("password", String(128), nullable=False)
-    start_date = Column("start_date", Date())
-    end_date = Column("end_date", Date())
+    # start_date = Column("start_date", Date())
+    # end_date = Column("end_date", Date())
+    target_time_period = Column('target_time_period', Text())
 
     # password.create(users)
-    start_date.create(groups)
-    end_date.create(groups)
+    # start_date.create(groups)
+    # end_date.create(groups)
+    target_time_period.create(groups)
     pass
