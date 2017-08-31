4c68e643ba824d3eee626ce68e6899b7df6b2e5f
maxbbender <maxbbender@gmail.com>
2017-08-31 16:59:53 -0400

    woops

--- db/versions/080_newsletterfix.py
+++ db/versions/080_newsletterfix.py
@@ -5,11 +5,6 @@ from migrate import *
 def upgrade(migrate_engine):
     meta = MetaData(bind=migrate_engine)
 
-    users = Table('users', meta, autoload=True)
-
-    # users.c.password.drop()
-    users.c.newsletter.drop()
-
     notifpref = Table('notification_preferences', meta, autoload=True)
 
     newsletter_email = Column("newsletter_email", Boolean(), nullable=True)
@@ -20,11 +15,6 @@ def upgrade(migrate_engine):
 
 def downgrade(migrate_engine):
     meta = MetaData(bind=migrate_engine)
-
-    # users.c.password.drop()
-    users = Table('users', meta, autoload=True)
-    newsletter = Column("newsletter", Boolean(), nullable=True)
-    newsletter.create(users)
 
     notifpref = Table('notification_preferences', meta, autoload=True)
 
